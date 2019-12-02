98732883ae5eddd784f392bd80e41a02351dfef5
Dunizb <zb@dunizb.vip>
2019-12-03 02:28:01 +0800

    add component

---
+++ miniprogram/components/text-ellipsis/index.js
@@ -0,0 +1,30 @@
+// src/components/text-ellipsis/index.js
+Component({
+  externalClasses: ['custom-class'],
+  options: {
+    addGlobalClass: true,
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    clamp: {
+      type: Number,
+      value: 1
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
